bf8211a795dff2d9409ce7baea7810c45d0f2d66
milli <gowrishankar.milli@gmail.com>
2017-08-22 00:18:57 +0530

    [osm-mongo] : added mongo store dependent sax handler and test cases

---
+++ osm-mongo/src/main/java/com/github/osm/mongo/sax/MongoOsmSaxHandler.java
@@ -0,0 +1,133 @@
+package com.github.osm.mongo.sax;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.xml.sax.SAXException;
+
+import com.github.osm.domain.Node;
+import com.github.osm.domain.Relation;
+import com.github.osm.domain.Way;
+import com.github.osm.mongo.OsmMongoStore;
+import com.github.osm.sax.OsmSaxHandler;
+
+
+/**
+ * A SAX parser event handler for OSM XML primarily intended for large XML files (typically of sizes
+ * more the 0.5GB). Instead of keeping the parsed element objects in memory, this uses a docstore to
+ * store them.
+ * 
+ * @see InMemoryOsmHandler
+ */
+public class MongoOsmSaxHandler extends OsmSaxHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(MongoOsmSaxHandler.class);
+
+    private final OsmMongoStore mongoStore;
+
+    private long nodesCount = 0;
+    private long waysCount = 0;
+    private long relationsCount = 0;
+
+
+    // Constructors
+    // ------------------------------------------------------------------------
+
+    public MongoOsmSaxHandler(OsmMongoStore mongoStore) {
+        super();
+
+        // Sanity checks
+        if (mongoStore == null) {
+            throw new IllegalArgumentException("MongoOsmSaxHandler :: mongo store is null");
+        }
+
+        // init
+        this.mongoStore = mongoStore;
+
+        LOGGER.info("Successfully Initialized new OsmHandler with MongoStore : {}", mongoStore);
+    }
+
+    // Getters and Setters
+    // ------------------------------------------------------------------------
+
+    @Override
+    public long getNodesCount() {
+        return nodesCount;
+    }
+
+    @Override
+    public long getWaysCount() {
+        return waysCount;
+    }
+
+    @Override
+    public long getRelationsCount() {
+        return relationsCount;
+    }
+
+
+    // SAX Handler methods
+    // ------------------------------------------------------------------------
+
+    @Override
+    public void startDocument() throws SAXException {
+        super.startDocument();
+
+        // Reset
+        this.nodesCount = 0;
+        this.waysCount = 0;
+        this.relationsCount = 0;
+    }
+
+    @Override
+    public void documentCompleted() {
+        // OUTPUT
+        LOGGER.info("No of Nodes Read: {}", this.nodesCount);
+        LOGGER.info("No of Ways Read: {}", this.waysCount);
+        LOGGER.info("No of Relations Read: {}", this.relationsCount);
+    }
+
+
+    // Handle OSM Elements
+    // ------------------------------------------------------------------------
+
+    @Override
+    public void handleNode(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        // Save to DB
+        this.mongoStore.insert(node);
+
+        this.nodesCount++;
+        LOGGER.info("Created new Node in the DB : {}", node);
+    }
+
+    @Override
+    public void handleWay(Way way) {
+        if (way == null) {
+            return;
+        }
+
+        // Save to DB
+        this.mongoStore.insert(way);
+
+        this.waysCount++;
+        LOGGER.info("Created new Way in the DB : {}", way);
+    }
+
+    @Override
+    public void handleRelation(Relation relation) {
+        if (relation == null) {
+            return;
+        }
+
+        // Save to DB
+        this.mongoStore.insert(relation);
+
+        this.relationsCount++;
+        LOGGER.info("Created new Relation in the DB : {}", relation);
+    }
+
+
+}
---
+++ osm-mongo/src/test/java/com/github/osm/mongo/sax/MongoOsmSaxHandlerTest.java
@@ -0,0 +1,98 @@
+package com.github.osm.mongo.sax;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.text.ParseException;
+import java.util.Map;
+
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.github.osm.domain.Node;
+import com.github.osm.mongo.OsmMongoStore;
+import com.github.osm.mongo.OsmMongoStoreTest;
+import com.github.osm.mongo.helper.MongoConfig;
+import com.github.osm.reader.OsmXmlReader;
+import com.github.osm.sax.OsmSaxHandler;
+
+
+public class MongoOsmSaxHandlerTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(OsmMongoStoreTest.class);
+
+    private OsmMongoStore _mongoStore;
+    private OsmSaxHandler _osmSaxHandler;
+
+    private OsmXmlReader _osmXmlReader;
+
+    private String _osmXml_sample1;
+    private String _osmXml_sample2;
+
+
+    // Setup
+    // ------------------------------------------------------------------------
+
+    @Before
+    public void setup() throws ParseException {
+        // init
+        final MongoConfig config = MongoConfig.with("localhost", 27017, "mongo_java_test");
+        _mongoStore = OsmMongoStore.withConfig(config);
+
+        _osmXmlReader = new OsmXmlReader();
+        _osmSaxHandler = new MongoOsmSaxHandler(_mongoStore);
+
+
+        // files
+        _osmXml_sample1 = "src/test/resources/samples/osm_node.xml";
+        _osmXml_sample2 = "src/test/resources/samples/osm_bbox.xml";
+    }
+
+    @After
+    public void teardown() {
+        // nothing to do
+    }
+
+
+    // Tests
+    // ------------------------------------------------------------------------
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_readOsmXml_null_filepath() {
+        _osmXmlReader.read(_osmSaxHandler, null);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void test_readOsmXml_empty_filepath() {
+        _osmXmlReader.read(null, null);
+    }
+
+    @Test
+    public void test_readOsmXml_valid_file1() throws FileNotFoundException {
+        // Stream
+        final InputStream is = new FileInputStream(_osmXml_sample1);
+        Map<String, Object> result = _osmXmlReader.read(_osmSaxHandler, is);
+        LOGGER.info("OsmXMLread Result for - {} is : {}", _osmXml_sample1, result);
+
+        Assert.assertTrue(result != null && result.size() > 0);
+        Assert.assertEquals(1, _osmSaxHandler.getNodesCount());
+
+        Node node = _mongoStore.findOne(Node.class, 3233393892L);
+        Assert.assertEquals(3233393892L, node.getOsmId());
+    }
+
+    @Test
+    public void test_readOsmXml_valid_file2() throws FileNotFoundException {
+        // Stream
+        final InputStream is = new FileInputStream(_osmXml_sample2);
+        Map<String, Object> result = _osmXmlReader.read(_osmSaxHandler, is);
+        LOGGER.info("OsmXMLread Result for - {} is : {}", _osmXml_sample2, result);
+
+        Assert.assertTrue(result != null && result.size() > 0);
+    }
+
+}
